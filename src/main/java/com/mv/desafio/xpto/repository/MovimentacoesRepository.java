cb9285d0f1fba5c14e793ae39a4bb8077eee13a8
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-25 02:41:21 -0300

    feat: criar repository para o model de movimentaçoes

---
+++ src/main/java/com/mv/desafio/xpto/repository/MovimentacoesRepository.java
@@ -0,0 +1,9 @@
+package com.mv.desafio.xpto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mv.desafio.xpto.model.Movimentacoes;
+
+public interface MovimentacoesRepository extends JpaRepository<Movimentacoes, Long>{
+
+}
