d59cd3ab4d0e30ff6081d6fb35dc6cbc41ef5a4d
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-26 21:07:32 -0300

    feat: incluir query em JPQL para retornar o quantativo de movimentações dos clientes

--- src/main/java/com/mv/desafio/xpto/repository/MovimentacoesRepository.java
+++ src/main/java/com/mv/desafio/xpto/repository/MovimentacoesRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.repository.query.Param;
 
 import com.mv.desafio.xpto.model.Movimentacoes;
 
-public interface MovimentacoesRepository extends JpaRepository<Movimentacoes, Long>{
-	
+public interface MovimentacoesRepository extends JpaRepository<Movimentacoes, Long> {
+	@Query("SELECT COUNT(m.id) FROM Movimentacoes m " +
+	           "JOIN m.conta c " +
+	           "JOIN c.cliente cl " +
+	           "WHERE c.dataCriacao BETWEEN CURRENT_DATE AND CURRENT_DATE + 30 " +
+	           "AND cl.id = :clienteId")
+	    Integer contarMovimentacoesPorClienteEPeriodo(@Param("clienteId") Long clienteId);
 }
